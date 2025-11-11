95ee12a73f70d5d58998d320ee7426a4119098d5
Gaurav N2 <gauravnehare1411@gmail.com>
2025-11-11 18:05:39 +0530

    change api url

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   }, []);
 
   return (
-    <Router basename='app'>
+    <Router basename='portal'>
       <AppNavbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} userRoles={userRoles} setUserRoles={setUserRoles}/>
       <Container style={{ paddingTop: "80px" }}>
         <Routes>
